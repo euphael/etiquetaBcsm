ad6f5a57a7854a8a7901a4c7745fbe9f8691022f
AndreBarbosa <andrevictorbarbosa@hotmail.com>
2025-09-04 10:47:07 -0300

    gestaoorcamentos

--- etiquetas/src/App.js
+++ etiquetas/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Etiquetas from './components/Etiquetas/Etiquetas';
 import EtiquetasLoja from './components/EtiquetasLoja/EtiquetasLoja';
+import GestaoOrcamento from './components/GestaoOrcamento/GestaoOrcamento';
 
 // import PlanejamentoProd from './components/PlanejamentoProd/PlanejamentoProd';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,6 +19,7 @@ const App = () => {
           <Routes>
             <Route path="/etiquetas" element={<Etiquetas />} />
             <Route path="/etiquetasLoja" element={<EtiquetasLoja />} />
+            <Route path="/GestaoOrcamento" element={<GestaoOrcamento />} />
 
             {/* <Route path="/PlanejamentoProd" element={<PlanejamentoProd />} /> */}
           </Routes>
