cf31d92ba67cad40c8c3157dbb1e3488a5db54dc
AndreBarbosa <andrevictorbarbosa@hotmail.com>
2025-09-09 11:18:50 -0300

    teste

--- etiquetas/src/App.js
+++ etiquetas/src/App.js
@@ -1,29 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Etiquetas from './components/Etiquetas/Etiquetas';
 import EtiquetasLoja from './components/EtiquetasLoja/EtiquetasLoja';
 import GestaoOrcamento from './components/GestaoOrcamento/GestaoOrcamento';
-
-// import PlanejamentoProd from './components/PlanejamentoProd/PlanejamentoProd';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import './App.css';
 
 const App = () => {
-
   return (
-    <div >
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/etiquetas" element={<Etiquetas />} />
-            <Route path="/etiquetasLoja" element={<EtiquetasLoja />} />
-            <Route path="/gestao-orcamento" element={<GestaoOrcamento />} />
-            {/* <Route path="/PlanejamentoProd" element={<PlanejamentoProd />} /> */}
-          </Routes>
-        </Router>
-      </div>
+    <div className="App">
+      <Router basename="/">
+        <Routes>
+          <Route path="/etiquetas" element={<Etiquetas />} />
+          <Route path="/etiquetasLoja" element={<EtiquetasLoja />} />
+          <Route path="/gestao-orcamento" element={<GestaoOrcamento />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
